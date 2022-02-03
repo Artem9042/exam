ee3cec99ae7c2590969a02018d892b9dc5342e9e
Dmytro <=>
2022-02-03 14:10:40 +0200

    initial commit

---
+++ Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	cout << "login" << "\n";
+	string login;
+	string password;
+	string admin_login = "login1234";
+	string admin_password = "admin1234";
+	cout << "enter login" << "\n";
+	cin >> login;
+	cout << "enter password" << "\n";
+	cin >> password;
+	if (admin_login && admin_password == true)
+	{
+
+	}
+	return 0;
+}
